caf2953f681f2c139173eca4f000acb3245f94c5
Mustafa Raza <mustafarazaullah@gmail.com>
2026-03-30 16:23:44 +0500

    Refactor code structure for improved readability and maintainability

--- lib/modules/onboarding/view/onboarding_view.dart
+++ lib/modules/onboarding/view/onboarding_view.dart
@@ -6,14 +6,78 @@ class OnboardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SizedBox.expand(
-        child: Column(children: [
-          Text("All your favorites"),
-          Text("Get all your loved foods in one once place, you just place the orer we do the rest"),
-          Text("NEXT"),
-          Text("Skip"),
-        ],),
-      )
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("assets/onboarding/onboarding_1.png", height: 450),
+              Text(
+                "All your favorites",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Get all your loved foods in one once place, you just place the orer we do the rest",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildDot(),
+                    SizedBox(width: 5),
+                    _buildDot(isActive: true),
+                    SizedBox(width: 5),
+                    _buildDot(),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFF7622),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Skip",
+                    style: TextStyle(fontSize: 15, color: Colors.blueGrey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDot({bool isActive = false}) {
+    return Container(
+      height: 10,
+      width: 10,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.orange : Colors.orange.withValues(alpha: 0.3),
+        borderRadius: BorderRadius.circular(100),
+      ),
     );
   }
 }
