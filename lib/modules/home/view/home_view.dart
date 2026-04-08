42e8bc5520bc56f8b08f33e3f9c1764e4f2267ea
Mustafa Raza <mustafarazaullah@gmail.com>
2026-04-08 16:25:21 +0500

    Add RestaurantTile component for displaying restaurant details in HomeView

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/common/app_TextField.dart';
+import 'package:store_app/modules/home/view/components/restaurant_tile.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,132 +12,225 @@ class HomeView extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          SizedBox(height: 50),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
               children: [
-                SizedBox(height: 50),
-                Row(
-                  children: [
-                    Container(
-                      height: 45,
-                      width: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withValues(alpha: 0.3),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: Center(child: Icon(Icons.menu, size: 25)),
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Deliver to",
-                          style: TextStyle(
-                            color: Colors.deepOrange,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text("Halal Lab Office"),
-                            Icon(Icons.arrow_drop_down, size: 23),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    Badge(
-                      label: Text(
-                        "2",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                      padding: EdgeInsets.all(2),
-                      backgroundColor: Colors.deepOrange,
-                      offset: Offset(-5, 0),
-                      child: Container(
-                        height: 45,
-                        width: 45,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: Center(
-                          child: Icon(
-                            Icons.store,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                Container(
+                  height: 45,
+                  width: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withValues(alpha: 0.3),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Center(child: Icon(Icons.menu, size: 25)),
                 ),
-                SizedBox(height: 20),
-                Row(
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "Hey Halal,",
+                      "Deliver to",
                       style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      " Good Afternoon",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                AppTextfield(
-                  hintText: "Search Dishes and Resturants",
-                  textFieldType: AppTextFieldType.search,
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Text(
-                      "All Categories",
-                      style: TextStyle(
-                        fontSize: 18,
+                        color: Colors.deepOrange,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    Spacer(),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "See All",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
+                    Row(
+                      children: [
+                        Text("Halal Lab Office"),
+                        Icon(Icons.arrow_drop_down, size: 23),
+                      ],
                     ),
-                    Icon(Icons.arrow_forward_ios_rounded, size: 15),
                   ],
+                ),
+                Spacer(),
+                Badge(
+                  label: Text(
+                    "2",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  padding: EdgeInsets.all(2),
+                  backgroundColor: Colors.deepOrange,
+                  offset: Offset(-5, 0),
+                  child: Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Center(
+                      child: Icon(Icons.store, size: 25, color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          SizedBox(
-            height: 80,
+          Expanded(
             child: ListView(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.fromLTRB(16, 10, 0, 10),
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
               children: [
-                _buildCategoryChip(true, "assets/icons/fire.png", "All"),
-                _buildCategoryChip(false, "assets/icons/fire.png", "Burger"),
-                _buildCategoryChip(false, "assets/icons/fire.png", "Pizza"),
-                _buildCategoryChip(false, "assets/icons/fire.png", "Sushi"),
-                _buildCategoryChip(false, "assets/icons/fire.png", "Dessert"),
-                _buildCategoryChip(false, "assets/icons/fire.png", "Drinks"),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Hey Halal,",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            " Good Afternoon",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      AppTextfield(
+                        hintText: "Search Dishes and Resturants",
+                        textFieldType: AppTextFieldType.search,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "All Categories",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "See All",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded, size: 15),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.fromLTRB(16, 10, 0, 10),
+                    children: [
+                      _buildCategoryChip(true, "assets/icons/fire.png", "All"),
+                      _buildCategoryChip(
+                        false,
+                        "assets/icons/fire.png",
+                        "Burger",
+                      ),
+                      _buildCategoryChip(
+                        false,
+                        "assets/icons/fire.png",
+                        "Pizza",
+                      ),
+                      _buildCategoryChip(
+                        false,
+                        "assets/icons/fire.png",
+                        "Sushi",
+                      ),
+                      _buildCategoryChip(
+                        false,
+                        "assets/icons/fire.png",
+                        "Dessert",
+                      ),
+                      _buildCategoryChip(
+                        false,
+                        "assets/icons/fire.png",
+                        "Drinks",
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Open Restaurants",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "See All",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded, size: 15),
+                        ],
+                      ),
+                      ListView(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: [
+                          RestaurantTile(
+                            imageUrl:
+                                "https://i.pinimg.com/1200x/75/15/6b/75156b85e4206ce567a9a0d85c387ef0.jpg",
+                            name: "Pizza Hut",
+                            description: "Delicious Pizza and more",
+                            rating: "4.5",
+                            distance: "2.3 km",
+                            deliveryTime: "30-40 min",
+                          ),
+
+                          RestaurantTile(
+                            imageUrl:
+                                "https://i.pinimg.com/736x/04/67/d2/0467d2e64a67a76ef2b275f6965669d9.jpg",
+                            name: "Bonfire",
+                            description: "Grilled to perfection",
+                            rating: "5",
+                            distance: "2.3 km",
+                            deliveryTime: "30-40 min",
+                          ),
+
+                          RestaurantTile(
+                            imageUrl:
+                                "http://i.pinimg.com/1200x/33/27/94/332794b629efc429e589141b0b045f1b.jpg",
+                            name: "Moye Moye Restaurant",
+                            description: "Authentic Nigerian Cuisine",
+                            rating: "2.5",
+                            distance: "2.3 km",
+                            deliveryTime: "30-40 min",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
